02005d066364ed47a1d76574d05d110aa84bf5a8
Daria Daniyalian <daria.daniyalian@footlocker.com>
2017-09-25 19:14:22 -0500

    odd and even num

---
+++ Algos.js
@@ -0,0 +1,27 @@
+
+//Print Num from 0 to 256;
+function Print(){
+    for (var i=0; i<256; i++){
+    console.log(i);
+  }
+
+}
+Print();
+
+//Print All Odd and Even Num;
+
+function printEven() {
+        for (var i = 0; i < 10; i++){
+            if ((i % 2) == 0)
+                console.log (i + ' ');
+        }
+    }
+
+    function printOdd() {
+        for (var i = 0; i < 10; i++){
+            if ((i % 2) !== 0) 
+                console.log(i + ' ');
+        }
+    }
+printEven();
+printOdd();
